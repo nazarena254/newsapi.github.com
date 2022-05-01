e5599f9138e85053e9a038bea7ecbc59dd6abf9c
nancyngunjiri1 <nancyngunjiri1@gmail.com>
2022-05-01 09:50:23 +0300

    add a navbar with links that redirects to other html pages

--- app/config.py
+++ app/config.py
@@ -1,14 +1,13 @@
 from distutils.log import DEBUG
 
 # we assign values to configuration variables that will be used by the app Eg ApiKey
-class Config: 
-    pass  
-    # NEWS_BASE_URL_SOURCES = 'https://newsapi.org/v2/top-headlines/sources?apiKey={}'
-    # NEWS_BASE_EVERYTHING_URL = 'https://newsapi.org/v2/everything?domains={}&apiKey={}'
-    # NEWS_BASE_HEADLINES_URL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey={}'
-    # NEWS_BASE_SOURCE = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+class Config:  
+    NEWS_BASE_URL_SOURCES = 'https://newsapi.org/v2/top-headlines/sources?apiKey={}'
+    NEWS_BASE_EVERYTHING_URL = 'https://newsapi.org/v2/everything?domains={}&apiKey={}'
+    NEWS_BASE_HEADLINES_URL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey={}'
+    NEWS_BASE_SOURCE = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
 
-    # API_KEY = "37a78df4920142b683f679fffd728ab9"
+    API_KEY = "37a78df4920142b683f679fffd728ab9"
 class ProdConfig(Config):
     pass
 
--- app/request.py
+++ app/request.py
@@ -1,5 +1,7 @@
 from .config import Config
 import urllib.request,json
 
+
+#The file makes requests to Api, request object holds all incoming data in flask frm the request
 api_key=None
 base_url=None
