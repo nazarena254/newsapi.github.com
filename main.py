737c1e069f27869d8ae9789b03eac0c42e9e8270
nancyngunjiri1 <nancyngunjiri1@gmail.com>
2022-05-01 12:14:10 +0300

    change the navbar class from beingfixed on the top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from distutils.log import debug
 from app import app
 
-# this file is used to run our app
+# this file is used to run our app news
 if __name__ == "__main__":
     app.run(debug = True)
