952ccb37e196ed60c49bff22d25fac352734f172
nancyngunjiri1 <nancyngunjiri1@gmail.com>
2022-05-01 10:07:56 +0300

    add publishedArticle class in request.py file

--- app/request.py
+++ app/request.py
@@ -1,7 +1,48 @@
 from .config import Config
 import urllib.request,json
-
+from .models import Articles
+from .models import Sources
+from newsapi import NewsApiClient
 
 #The file makes requests to Api, request object holds all incoming data in flask frm the request
 api_key=None
 base_url=None
+base_url_for_everything=None
+base_url_top_headlines=None
+base_source_list=None
+
+def publishedArticles():
+    newsapi = NewsApiClient(api_key= Config.API_KEY)
+
+    get_articles = newsapi.get_everything(sources= 'cnn, reuters, cnbc, the-verge, gizmodo, the-next-web, techradar, recode, ars-technica')
+
+    all_articles = get_articles['articles']
+
+    articles_results = []
+
+    source = []
+    title = []
+    desc = []
+    author = []
+    img = []
+    p_date = []
+    url = []
+
+    for i in range(len(all_articles)):
+        article = all_articles[i]
+
+        source.append(article['source'])
+        title.append(article['title'])
+        desc.append(article['description'])
+        author.append(article['author'])
+        img.append(article['urlToImage'])
+        p_date.append(article['publishedAt'])
+        url.append(article['url'])
+
+        article_object = Articles(source, title, desc, author, img, p_date, url)
+
+        articles_results.append(article_object)
+
+        contents = zip(source, title, desc, author, img, p_date, url)
+
+    return  contents
